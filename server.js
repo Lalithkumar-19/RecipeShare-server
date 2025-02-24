ccb4d9de0af8ff1636557dfbd900510a532a1796
lalith kumar <lalithdev123@gmail.com>
2025-02-24 12:38:03 +0530

    latest changes on deletion recipe

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const PORT = process.env.PORT||5000;
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: 
+  "https://recipe-share-frontend-ten.vercel.app/" }));
 const LoginRouter = require("./Routes/LoginRoutes");
 const RecipeRouter = require("./Routes/RecipeRoutes");
 const UserRoutes = require("./Routes/UserRoutes");
@@ -21,6 +22,6 @@ app.use("/api",UserRoutes);
 
 
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
